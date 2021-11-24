ad68a0fe64c066f51bad4b020254d558300e047e
KirillSh22 <sarikovk38@gmail.com>
2021-11-24 21:03:00 +0300

    fix select line on start

--- Assets/Line Drawer/Scripts/TogglesSwitcher.cs
+++ Assets/Line Drawer/Scripts/TogglesSwitcher.cs
@@ -7,10 +7,15 @@ public class TogglesSwitcher : MonoBehaviour
 {
     internal List<ToggleItem> Toggles = new List<ToggleItem>();
 
-   /* private void Start()
+    private void Start()
     {
+        IEnumerable<ToggleItem> sortedToggles = from item in Toggles
+            orderby item._order ascending
+                            select item;
+        Toggles = sortedToggles.ToList();
+        Debug.Log(Toggles.Count);
         SwitchToggle(Toggles[0]);
-    }*/
+    }
 
     internal void SwitchToggle(ToggleItem toggleItem)
     {
@@ -29,6 +34,7 @@ public abstract class ToggleItem : MonoBehaviour
 {
     [SerializeField] protected TogglesSwitcher _togglesSwitcher;
     [SerializeField] protected GameObject _showHideObject;
+    public int _order;
 
 
     private void Awake()
